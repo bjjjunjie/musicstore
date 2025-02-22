b6da5aa5687faa8c3b27268c2b167540edab24ff
小白 <bjjjunjie@126.com>
2025-02-22 19:34:51 +0800

    创建图标资源

---
+++ AvaloniaApplication1/AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AvaloniaApplication1.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "文本";
+    public string Name => "World";
+
+
+    public string Age => "12";
+}
---
+++ AvaloniaApplication1/AvaloniaApplication1/Views/MainView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using System.Diagnostics;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Debug.WriteLine("Button Clicked");
+    }
+}
