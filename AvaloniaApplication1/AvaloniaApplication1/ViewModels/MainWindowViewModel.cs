683a2d8c43454ea746f76289d7a887f5e29f4885
小白 <bjjjunjie@qq.com>
2025-02-22 21:06:58 +0800

    列表绑定

---
+++ AvaloniaApplication1/AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AvaloniaApplication1.ViewModels
+{
+   public class MainWindowViewModel:ViewModelBase
+    {
+        public ICommand BuyMusicCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
+            //BuyMusicCommand = ReactiveCommand.Create(() =>
+            //{
+            //    // 当按钮被点击时，这里的代码将被执行。
+            //    Debug.WriteLine("购买音乐");
+            //});
+            BuyMusicCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var store = new MusicStoreViewModel();
+
+                var result = await ShowDialog.Handle(store);
+            });
+        }
+        public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+    }
+}
--- AvaloniaApplication1/AvaloniaApplication1/Views/MainWindow.axaml.cs
+++ AvaloniaApplication1/AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -1,11 +1,26 @@
 ﻿using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using AvaloniaApplication1.ViewModels;
+using ReactiveUI;
+using System.Threading.Tasks;
 
 namespace AvaloniaApplication1.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+       // DataContext = new MainWindowViewModel();
+        this.WhenActivated(action =>
+               action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
+    }
+    private async Task DoShowDialogAsync(InteractionContext<MusicStoreViewModel,AlbumViewModel?> interaction)
+    {
+        var dialog = new MusicStoreWindow();
+        dialog.DataContext = interaction.Input;
+
+        var result = await dialog.ShowDialog<AlbumViewModel?>(this);
+        interaction.SetOutput(result);
     }
 }
